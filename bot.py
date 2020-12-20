1de59d3ab227e279bde6ac96560fb32a7270e7fa
StevenDankMeister <stran19@student.aau.dk>
2020-12-20 20:20:38 +0100

    documentation

--- bot.py
+++ bot.py
@@ -10,12 +10,39 @@ bot = commands.Bot(command_prefix='>')
 
 
 def open_gamer_moments_file(guild_id, mode):
+    """Open gamer moments file for a guild
+
+    Args:
+        guild_id (int): id of the guild
+        mode (char*): mode to open the file
+
+    Returns:
+        file object: the opened file
+    """
     return open('./{}_quotes.json'.format(str(guild_id)), mode, encoding='utf-8')
 
 def open_gamer_words_file(guild_id, mode):
+    """Open gamer words file for a guild
+
+    Args:
+        guild_id (int): id of the guild
+        mode (char*): mode to open the file
+
+    Returns:
+        file object: the opened file
+    """
     return open('./{}_gamerwords.txt'.format(str(guild_id)), mode, encoding='utf-8')
 
-def get_gamer_moment_message(guild_id, user_name):
+def get_gamer_moment_message(guild_id, user_name=None):
+    """Get a gamer moment string from a guild
+
+    Args:
+        guild_id (int): id of the guild
+        user_name (char*, optional): name of user to get moment from. Defaults to None.
+
+    Returns:
+        char*: The gamer moment string
+    """
     f = open_gamer_moments_file(guild_id, 'r')
 
     moments_dic = json.load(f)
@@ -32,6 +59,12 @@ def get_gamer_moment_message(guild_id, user_name):
     return gamer_moment_sentence 
 
 def add_gamer_word(strings, guild_id):
+    """Add gamer word(s) to a guild
+
+    Args:
+        strings (char*): gamer word(s) to add
+        guild_id (int): id of the guild
+    """
     f = open_gamer_words_file(guild_id, 'a+')
     
     for string in strings:
@@ -41,6 +74,15 @@ def add_gamer_word(strings, guild_id):
     f.close()
 
 def get_all_gamer_words(guild):
+    
+    """Get all gamer words for a guild
+
+    Args:
+        guild (discord.Guild): guild to get gamer words from
+
+    Returns:
+        char**: A list of all gamer words in a guild
+    """
     gamer_words = []
     f = open_gamer_words_file(guild.id, 'r')
 
@@ -52,6 +94,11 @@ def get_all_gamer_words(guild):
     return gamer_words
 
 def remove_duplicate_gamer_words(guild):
+    """Remove duplicate gamer words for a guild
+
+    Args:
+        guild (discord.Guild): guild to remove gamer words from
+    """
     words = []
     
     f = open_gamer_words_file(guild.id, 'r+')
@@ -68,9 +115,23 @@ def remove_duplicate_gamer_words(guild):
     add_gamer_word(words, guild.id)
 
 def get_username_format(user):
+    """Get formatted username
+
+    Args:
+        user (discord.User): the user to get username from
+
+    Returns:
+        char*: username with format name#discriminator
+    """
     return '{}#{}'.format(user.name, user.discriminator).lower()
 
 def remove_gamer_word(guild_id, word_to_remove):
+    """Remove a gamer word from a guild
+
+    Args:
+        guild_id (int): the guild to remove from
+        word_to_remove (char*): the word to remove
+    """
     f = open_gamer_words_file(guild_id, 'r')
 
     words = []
@@ -115,7 +176,7 @@ class GamerMoments(commands.Cog, name='Gamer Moments'):
             user_name = args[0]
             quote = get_gamer_moment_message(ctx.guild.id, user_name.lower())
         else:
-            quote = get_gamer_moment_message(ctx.guild.id, None)
+            quote = get_gamer_moment_message(ctx.guild.id)
 
         await ctx.send(quote)
 
